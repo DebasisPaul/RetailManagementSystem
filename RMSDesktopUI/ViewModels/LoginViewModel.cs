268b91c6050e17c1509a2ec654c97b175739008e
debasispaul <idebasispaul@gmail.com>
2022-09-24 18:43:30 -0700

    LogIn Form Error Handling.

--- RMSDesktopUI/ViewModels/LoginViewModel.cs
+++ RMSDesktopUI/ViewModels/LoginViewModel.cs
@@ -43,6 +43,36 @@ namespace RMSDesktopUI.ViewModels
             }
         }
 
+		public bool IsErrorVisible
+		{
+			get 
+			{ 
+				bool output = false;
+
+				if (ErrorMessage?.Length > 0)
+				{
+					output = true;
+				}
+				return output; 
+			}
+
+		}
+
+		private string _errorMessage;
+
+		public string ErrorMessage
+		{
+			get { return _errorMessage; }
+			set 
+			{
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+			}
+		}
+
+
+
 		public bool CanLogIn
 		{
 			get
@@ -62,11 +92,12 @@ namespace RMSDesktopUI.ViewModels
 		{
 			try
 			{
+				ErrorMessage = "";
 				var result = await _apiHelper.Authenticate(UserName, Password);
 			}
 			catch (Exception ex)
 			{
-				Console.WriteLine(ex.Message);
+				ErrorMessage = ex.Message;
 			}
 		}
 
