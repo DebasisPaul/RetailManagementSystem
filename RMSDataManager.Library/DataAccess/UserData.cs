91c5f98e5776876e4832c96ee4eea75315c07596
debasispaul <idebasispaul@gmail.com>
2022-09-25 11:47:25 -0700

    O

---
+++ RMSDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using RMSDataManager.Library.Internal.DataAccess;
+using RMSDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMSDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public  List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMSData");
+
+            return output;
+        }
+    }
+}
---
+++ RMSDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using RMSDataManager.Library.DataAccess;
+using RMSDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace RMSDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
